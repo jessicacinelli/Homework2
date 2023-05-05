3840e0e9be999fb9e91554e110de4441770784b1
jessicacinelli <jessicacinelli98@gmail.com>
2023-05-05 20:58:33 +0200

    Fix

--- Task1.R
+++ Task1.R
@@ -4,20 +4,19 @@
 #estimate the number of clusters based on the results of hclust and kmeans; 
 # discuss obtained results.
 
+#library(factoextra)
 #lettura dei dati flows.csv
 data<- read.csv('flows/flows1.csv')
-data
-
 
 ################## CLUSTERING GERARCHICO ################## 
 
 #Calcolo delle distanze euclidee:
 # restituisce la matrice di distanze euclidee, cioè tutte le possibili distanze tra coppie di punti. 
-d<- dist(data, method="euclidean")
+distances<- dist(data, method="euclidean")
 
 # algoritmo standard di clustering gerarchico:
 #"ward.D" "single", "complete", "average" (= UPGMA), "mcquitty" (= WPGMA), "median" (= WPGMC) or "centroid" (= UPGMC).
-fit <- hclust(d, method="ward.D")
+fit <- hclust(distances, method="ward.D")
 
 #plot del dendrogramma
 plot(fit)
@@ -31,11 +30,16 @@ rect.hclust(fit, k=3, border = "blue")
 
 #Taglio dell'albero: 
 #l’uscita è fornita come identificativo del cluster di appartenenza per ogni punto.
-g<- cutree(fit, k=3)
-g
+groups<- cutree(fit, k=3)
+
+g <- hcut(data, k = 3,
+            hc_func = "hclust", hc_method = "ward")
+
+#g
+#fviz_cluster(g) plotta solo  due dimensioni
 
 #creazione dataframe
-clusters<-g
+clusters<-groups
 df<-clusters
 df
 #plot(data, col=g, pch=g, cex=2) 
@@ -43,12 +47,20 @@ df
 #distribuzione dei punti nei cluster
 #hist(g, breaks=5, xlab = "k", main=paste("Distribuzione dei punti nei cluster" ), labels = TRUE, ylim=c(0,2500))
 h<-hist(df, col="lightblue", labels = TRUE, 
-          breaks=seq(min(df)-1,max(df)),
-          axes=F, main="Distribuzione dei punti nei cluster", xlab="K")
+        breaks=seq(min(df)-1,max(df)),
+        axes=F, 
+        ylim=c(0,2500),
+        xlab="K",
+        main="Distribuzione dei punti nei cluster")
 axis(2)
+#stampa il cluster di ogni bin)
 axis(1,at=h$mids,seq(min(df),max(df)))
 
+
+
 ################## CLUSTERING K-MEANS ################## 
+#lettura dei dati flows.csv
+data<- read.csv('flows/flows1.csv')
 #definiamo un vettore di 10 elementi con un id crescente (tss=total squares sum): 
 #ipotizziamo di testare fino a 10 cluster (k=10)
 #tss è la metrica che vogliamo misurare in base al numero di cluster.
@@ -57,14 +69,14 @@ tss<-seq(1,10,1)
 # Test dei valori di k da 1 a 10.
 for (i in 1:10) tss[i] <- kmeans(data,i)$tot.withinss   
 tss   #-> è una metrica del totale della somma dei quadrati intra-cluster. Varia molto velocemente quando varia il numero di cluster.
-plot(tss, type='o')
-plot(tss, type="o")
+plot(tss, type='o', xlab="K")
+
 
 #l'idea è scegliere qualcosa che si trova in corripondenza del ginocchio o subito dopo il ginocchio.
 points( c(4), tss[which.min(tss>2e20)], pch=20, col="green", cex=2)
-
+points( c(3), tss[which.min(tss>8e20)], pch=20, col="blue", cex=2)
 # k=4 
-res<-kmeans(data,4) #non va bene perché restituisce un cluster costituito da un unico punto.
+res<-kmeans(data,3) #non va bene perché restituisce un cluster costituito da un unico punto.
 
 #restituisce l'id del cluster per i vari punti
 res$cluster 
@@ -85,9 +97,9 @@ hist(res$cluster, xlab = "k", main="Distribuzione dei punti nei cluster")
 
 h2<-hist(df_km, col="lightblue", labels = TRUE, 
         breaks=seq(min(df_km)-1,max(df_km)),
-        axes=F, main="Distribuzione dei punti nei cluster", xlab="K")
+        axes=F, main="Distribuzione dei punti nei cluster", xlab="K", ylim=c(0,2500))
 axis(2)
-axis(1,at=h$mids,seq(min(df),max(df)))
+axis(1,at=h2$mids,seq(min(df_km),max(df_km)))
 #plot(data, col=res$cluster, pch=res$cluster)
 
 # Aggiunta dei centroidi sul grafico
