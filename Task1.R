8850cc3b6e0d05b320c656c0502bc30a2e7d99fb
jessicacinelli <jessicacinelli98@gmail.com>
2023-05-15 18:36:45 +0200

    Task 4: eliminazione outlier e riesecuzione del clustering k-means

--- Task1.R
+++ Task1.R
@@ -1,10 +1,3 @@
-#First task: 
-#run both hclust and kmeans; 
-#produce all related plots (e.g., dendrogram, sensitivity of tot.withinss); 
-#estimate the number of clusters based on the results of hclust and kmeans; 
-# discuss obtained results.
-
-#library(factoextra)
 #lettura dei dati flows.csv
 data<- read.csv('flows/flows1.csv')
 
@@ -26,13 +19,13 @@ abline(h=1.3e11, col="red",  lty=2)
 #plot(dendrogramma, ylim = c(0,200000000000), labels = FALSE, hang=(-1))
 # utilizziamo la funzione rect.hclust per mettere i rettangoli
 #k è il numero di cluster che individuiamo guardando il dendrogramma
-rect.hclust(fit, k=3, border = "blue")
+rect.hclust(fit, k=4, border = "blue")
 legend( x="topright", 
         legend=c("Taglio delle partizioni", "k cluster"),
         col=c("red","blue"), lwd=1, cex=0.7)
 #Taglio dell'albero: 
 #l’uscita è fornita come identificativo del cluster di appartenenza per ogni punto.
-groups<- cutree(fit, k=4)
+groups<- cutree(fit, k=3)
 
 g <- hcut(data, k = 3,
             hc_func = "hclust", hc_method = "ward")
@@ -44,7 +37,7 @@ g <- hcut(data, k = 3,
 clusters<-groups
 df<-clusters
 df
-#plot(data, col=g, pch=g, cex=2) 
+plot(data, col=groups, pch=groups, cex=1) 
 
 #distribuzione dei punti nei cluster
 #hist(g, breaks=5, xlab = "k", main=paste("Distribuzione dei punti nei cluster" ), labels = TRUE, ylim=c(0,2500))
@@ -62,6 +55,7 @@ axis(1,at=h$mids,seq(min(df),max(df)))
 
 ################## CLUSTERING K-MEANS ################## 
 data<- read.csv('flows/flows1.csv')
+
 #definiamo un vettore di 10 elementi con un id crescente (tss=total squares sum): 
 #ipotizziamo di testare fino a 10 cluster (k=10)
 #tss è la metrica che vogliamo misurare in base al numero di cluster.
@@ -75,9 +69,9 @@ plot(tss, type='o', xlab="K")
 
 #l'idea è scegliere qualcosa che si trova in corripondenza del ginocchio o subito dopo il ginocchio.
 points( c(4), tss[which.min(tss>2e20)], pch=20, col="green", cex=2)
-points( c(3), tss[which.min(tss>8e20)], pch=20, col="blue", cex=2)
+
 # k=4 
-res<-kmeans(data,3) #non va bene perché restituisce un cluster costituito da un unico punto.
+res<-kmeans(data,4) #non va bene perché restituisce un cluster costituito da un unico punto.
 
 #restituisce l'id del cluster per i vari punti
 res$cluster 
@@ -90,19 +84,54 @@ res$withinss
 
 #creazione dataframe
 clusters_kmeans<-res$cluster
-df_km<-clusters_kmeans
-df_km
 
 #distribuzione dei punti nei cluster
 hist(res$cluster, xlab = "k", main="Distribuzione dei punti nei cluster")
 
-h2<-hist(df_km, col="lightblue", labels = TRUE, 
-        breaks=seq(min(df_km)-1,max(df_km)),
+h2<-hist(clusters_kmeans, col="lightblue", labels = TRUE, 
+        breaks=seq(min(clusters_kmeans)-1,max(clusters_kmeans)),
         axes=F, main="Distribuzione dei punti nei cluster", xlab="K", ylim=c(0,2500))
 axis(2)
-axis(1,at=h2$mids,seq(min(df_km),max(df_km)))
-#plot(data, col=res$cluster, pch=res$cluster)
+axis(1,at=h2$mids,seq(min(clusters_kmeans),max(clusters_kmeans)))
+plot(data, col=res$cluster, pch=res$cluster)
 
-# Aggiunta dei centroidi sul grafico
-#points(res$centers, pch=5) 
 
+## RIMOZIONE OUTLIER 
+clusters<-table(res$cluster)<10
+
+# Get the indices where the logical vector is TRUE
+cluster_index <- which(clusters)
+
+#rimozione del cluster con indice cluster_index dal dataset trasformato e dai risultati del kmeans
+rows_to_remove <- res$cluster == cluster_index
+data2<-subset(data, (res$cluster) != cluster_index)
+#data.translated2 <- data.translated[-rows_to_remove,]
+dim(data.translated2)
+res2<-res
+res2$centers <- res$centers[-cluster_index, ]
+res2$cluster <- res$cluster[!rows_to_remove]
+res2$size <- res$size[-cluster_index]
+res2$totss <- res$totss - sum(res$withinss[cluster_index])
+res2$withinss <- res$withinss[-cluster_index]
+res2$betweenss <- res$totss - sum(res$withinss)
+res2$iter <- res$iter + 1
+
+table(res2$cluster)
+
+
+#new distribution
+clusters_pca1<-res2$cluster
+clusters_pca1
+
+#distribuzione dei punti nei cluster
+h3<-hist(clusters_pca1, col="lightblue", labels = TRUE, 
+         breaks=seq(min(clusters_pca1)-1,max(clusters_pca1)),
+         axes=F, main="Distribuzione dei punti nei cluster: scale = FALSE", xlab="K", ylim=c(0,2500))
+axis(2)
+axis(1,at=h3$mids,seq(min(clusters_pca1),max(clusters_pca1)))
+
+
+
+sequence<-seq(min(clusters_pca1)-1,max(clusters_pca1))
+my_seq_no_zero <- sequence[sequence != 0]
+my_seq_no_zero
--- Task4_Scale=FALSE.R
+++ Task4_Scale=FALSE.R
@@ -61,40 +61,46 @@ clusters<-table(res$cluster)<10
 # Get the indices where the logical vector is TRUE
 cluster_index <- which(clusters)
 
-#rimozione del cluster con indice cluster_index dal dataset trasformato e dai risultati del kmeans
-rows_to_remove <- res$cluster == cluster_index
+#rimozione del cluster con indice cluster_index dal dataset trasformato 
 data.translated2<-subset(data.translated, (res$cluster) != cluster_index)
+
 #data.translated2 <- data.translated[-rows_to_remove,]
 dim(data.translated2)
-res2<-res
-res2$centers <- res$centers[-cluster_index, ]
-res2$cluster <- res$cluster[!rows_to_remove]
-res2$size <- res$size[-cluster_index]
-res2$totss <- res$totss - sum(res$withinss[cluster_index])
-res2$withinss <- res$withinss[-cluster_index]
-res2$betweenss <- res$totss - sum(res$withinss)
-res2$iter <- res$iter + 1
 
-table(res2$cluster)
+plot(data.translated2, main = "Dataset traslato: scale = TRUE")
+
+############ k means
+tss<-seq(1,10,1)  
+
+# Test dei valori di k da 1 a 10.
+for (i in 1:10) tss[i] <- kmeans(data.translated2,i)$tot.withinss   
+tss   #-> è una metrica del totale della somma dei quadrati intra-cluster. Varia molto velocemente quando varia il numero di cluster.
+plot(tss, type='o', xlab="K", main="Numero ottimale di cluster: scale = FALSE")
 
 
-#new distribution
-clusters_pca1<-res2$cluster
-clusters_pca1
+#l'idea è scegliere qualcosa che si trova in corripondenza del ginocchio o subito dopo il ginocchio.
+points( c(4), tss[which.max(tss<0.6e20)], pch=20, col="green", cex=2) #scale=TRUE
+
+res<-kmeans(data.translated2,4) #non va bene perché restituisce un cluster costituito da un unico punto.
+
+clusters_kmeans<-res$cluster
 
 #distribuzione dei punti nei cluster
-h3<-hist(clusters_pca1, col="lightblue", labels = TRUE, 
-         breaks=seq(min(clusters_pca1)-1,max(clusters_pca1)),
+h2<-hist(clusters_kmeans, col="lightblue", labels = TRUE, 
+         breaks=seq(min(clusters_kmeans)-1,max(clusters_kmeans)),
          axes=F, main="Distribuzione dei punti nei cluster: scale = FALSE", xlab="K", ylim=c(0,2500))
 axis(2)
-axis(1,at=h3$mids,seq(min(clusters_pca1),max(clusters_pca1)))
+axis(1,at=h2$mids,seq(min(clusters_kmeans),max(clusters_kmeans)))
 
-#plot del dataset senza outlier
-fviz_cluster(res2, data.translated2,  geom = c("point"), 
-              repel=TRUE,  stand=FALSE,
-              #xlim=c(-3.5e9,5e8), ylim=c(-1e8,1e8),
-              title="Cluster plot: scale = FALSE") +
-              theme(plot.title = element_text(hjust = 0.5))+
-              geom_point(data = as.data.frame(res2$centers), 
-              aes(x = res2$centers[,1], y =res2$centers[,2] ), 
-              size = 5, shape = 8)
+# Visualize clusters
+fviz_cluster(res, data.translated2,  geom = c("point"), 
+             repel=TRUE,  stand=FALSE,
+             #xlim=c(-5,5), ylim=c(-12,3),
+             title="Cluster plot: scale = FALSE") +
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  geom_point(data = as.data.frame(res$centers), 
+             aes(x = res$centers[,1], y =res$centers[,2] ), 
+             size = 5, shape = 8)
+
+
+
